ea76d13e179c47996f70d6a4887b8c5ab9680008
Yuan Tian <tian@Yuans-Mac-mini.local>
2022-02-03 23:29:12 +0000

    Added champ.prepareManifest

---
+++ R/champ.prepareManifest.R
@@ -0,0 +1,31 @@
+# Function to preprocess manifest provided by illumina (or other valid format)
+# into a data frame for IDAT data loading. More specifically, it will extract
+# information like probe, CpG, colour channel, control .etc from manifest. They
+# are important for IDAT file parsing.
+
+champ.prepareManifest <- function(manifest_path, skip_mark = "[Assay]") {
+  manifest <- read.csv(manifest_path, head = TRUE, sep = ",")
+  manifest <- manifest[, c(1, 3, 5, 7, 9)]
+
+  type_II <- manifest[which(manifest$Infinium_Design_Type == "II"), ]
+  type_I_Red <- manifest[which(manifest$Infinium_Design_Type == "I" &
+    manifest$Color_Channel == "Red"), ]
+  type_I.Grn <- manifest[which(manifest$Infinium_Design_Type == "I" &
+    manifest$Color_Channel == "Grn"), ]
+
+  M.name <- c(as.character(Type_II[, 1]), as.character(Type_I.Red[, 1]), as.character(Type_I.Grn[, 1]))
+  M.index <- c(paste("G", Type_II[, 2], sep = "-"), paste("R", Type_I.Red[, 3], sep = "-"), paste("G", Type_I.Grn[, 3], sep = "-"))
+  U.index <- c(paste("R", Type_II[, 2], sep = "-"), paste("R", Type_I.Red[, 2], sep = "-"), paste("G", Type_I.Grn[, 2], sep = "-"))
+  P.Channal <- c(rep("g+r", nrow(Type_II)), rep("r", nrow(Type_I.Red)), rep("g", nrow(Type_I.Grn)))
+
+  Annotation <- data.frame(CpG = M.name, M.index = M.index, U.index = U.index, Channel = P.Channal)
+
+  rownames(Annotation) <- Annotation$CpG
+
+  Annotation$CpG <- as.character(Annotation$CpG)
+  Annotation$M.index <- as.character(Annotation$M.index)
+  Annotation$U.index <- as.character(Annotation$U.index)
+  Annotation$Channel <- as.character(Annotation$Channel)
+
+  Control <- read.csv("MethylationEPIC_v-1-0_B4_3.csv", head = F, sep = ",")
+}
