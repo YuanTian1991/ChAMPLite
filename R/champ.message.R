0710a55526bf3e2d2a7977f459c554e1dec013f7
Yuan Tian <tian@yuans-mac-mini.home>
2022-02-05 19:21:37 +0000

    PrepareManifest nearly done

--- R/champ.message.R
+++ R/champ.message.R
@@ -1,8 +1,7 @@
-champ.message <- function(tabs = 1, msg, type = 'message')
-{
-    output <- ""
-    for(i in 1:tabs) output <- paste(output,"  ")
-    output <- paste(output, msg)
+champ.message <- function(tabs = 1, msg, type = "message") {
+  output <- ""
+  for (i in 1:tabs) output <- paste(output, "  ")
+  output <- paste(output, msg)
 
-    if(type == "stop") stop(output) else message(output)
+  if (type == "stop") stop(output) else message(output)
 }
--- R/champ.prepareManifest.R
+++ R/champ.prepareManifest.R
@@ -3,24 +3,43 @@
 # information like probe, CpG, colour channel, control .etc from manifest. They
 # are important for IDAT file parsing.
 
-champ.prepareManifest <- function(manifest_path, skip_mark = "[Assay]") {
-  manifest <- read.csv(manifest_path, head = TRUE, sep = ",")
-  manifest <- manifest[, c(1, 3, 5, 7, 9)]
+library("data.table")
 
-  type_II <- manifest[which(manifest$Infinium_Design_Type == "II"), ]
-  type_I_Red <- manifest[which(manifest$Infinium_Design_Type == "I" &
-    manifest$Color_Channel == "Red"), ]
-  type_I.Grn <- manifest[which(manifest$Infinium_Design_Type == "I" &
-    manifest$Color_Channel == "Grn"), ]
+champ.PrepareManifest <- function(manifestPath,
+                                  skip = "auto") {
+    
+    champ.paramCheck.PrepareManifest(manifestPath, skip)
 
-  M.name <- c(as.character(Type_II[, 1]), as.character(Type_I.Red[, 1]), as.character(Type_I.Grn[, 1]))
-  M.index <- c(paste("G", Type_II[, 2], sep = "-"), paste("R", Type_I.Red[, 3], sep = "-"), paste("G", Type_I.Grn[, 3], sep = "-"))
-  U.index <- c(paste("R", Type_II[, 2], sep = "-"), paste("R", Type_I.Red[, 2], sep = "-"), paste("G", Type_I.Grn[, 2], sep = "-"))
-  P.Channal <- c(rep("g+r", nrow(Type_II)), rep("r", nrow(Type_I.Red)), rep("g", nrow(Type_I.Grn)))
+    skip_lines <- NULL
+    if(skip == "auto") {
+        manifest <- fread(manifestPath, fill=TRUE, showProgress=FALSE)
+        skip_lines$assay <- grep('\\[Assay\\]', as.data.frame(manifest[, 1])[,1])
+        skip_lines$controls <- grep('\\[Controls\\]', as.data.frame(manifest[, 1])[,1])
+    } else {
+        skip_lines$assay <- skip[1]
+        skip_lines$controls <- skip[2]
+    }
 
-  Annotation <- data.frame(CpG = M.name, M.index = M.index, U.index = U.index, Channel = P.Channal)
+  manifest <- fread(manifestPath, head = TRUE, skip=skip_lines$assay, nrows=skip_lines$controls - skip_lines$assay - 2)
+  manifest <- manifest[, c("IlmnID", "AddressA_ID", "AddressB_ID", "Infinium_Design_Type", "Color_Channel")]
 
-  rownames(Annotation) <- Annotation$CpG
+  typeII <- manifest[Infinium_Design_Type == "II", ]
+  typeIRed <- manifest[Infinium_Design_Type == "I" & Color_Channel == "Red", ]
+  typeIGrn <- manifest[Infinium_Design_Type == "I" & Color_Channel == "Grn", ]
+
+  mName <- c(typeII$IlmnID, typeIRed$IlmnID, typeIGrn$IlmnID)
+  mIndex <- c(paste("G", typeII$AddressA_ID, sep = "-"), 
+              paste("R", typeIRed$AddressB_ID, sep = "-"), 
+              paste("G", typeIGrn$AddressB_ID, sep = "-"))
+
+  uIndex <- c(paste("R", typeII$AddressA_ID, sep = "-"), 
+              paste("R", typeIRed$AddressA_ID, sep = "-"), 
+              paste("G", typeIGrn$AddressA_ID, sep = "-"))
+
+  pChannal <- c(rep("g+r", nrow(typeII)), rep("r", nrow(typeIRed)), rep("g", nrow(typeIGrn)))
+
+  probeInfo <- data.frame(CpG = mName, mIndex = mIndex, uIndex = uIndex, channel = pChannal)
+  rownames(probeInfo) <- probeInfo$CpG
 
   Annotation$CpG <- as.character(Annotation$CpG)
   Annotation$M.index <- as.character(Annotation$M.index)
@@ -28,4 +47,21 @@ champ.prepareManifest <- function(manifest_path, skip_mark = "[Assay]") {
   Annotation$Channel <- as.character(Annotation$Channel)
 
   Control <- read.csv("MethylationEPIC_v-1-0_B4_3.csv", head = F, sep = ",")
+}
+
+
+champ.paramCheck.PrepareManifest <- function(manifestPath, skip) {
+    if(class(manifestPath) !== "character") {
+        stop("[Parameter Error] parameter manifest_path must be a string character to manifest path.")
+    }
+
+    if(!file.exists(manifestPath)) {
+        stop("[Parameter Error] manifest file not found in manifest_path. Please check your file path.")
+    }
+
+    if(skip != "auto" & (class(skip) != "numeric" & length(skip) == 2)) {
+        stop("[Parameter Error] array_skip must be 'auto' or a two length integer vector.")
+    }
+
+    message("[Parameter Check Success]")
 }
